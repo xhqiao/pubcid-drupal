e492f0ba25fda8e2cf03afae71d44caa36ff3602
xqiao <xqiao@conversantmedia.com>
2019-05-16 14:50:42 -0700

    Update cookie domain field and samesite cookie option

--- src/Controller/PubcidCookieManagementController.php
+++ src/Controller/PubcidCookieManagementController.php
@@ -4,96 +4,98 @@ namespace Drupal\pubcid_cookie_management\Controller;
 
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
+const DAYS_IN_SECONDS = 86400;
+
+/**
+ * Class PubcidCookieManagementController
+ * @package Drupal\pubcid_cookie_management\Controller
+ * Setting cookie Based on the pubcid configurations
+ */
 class PubcidCookieManagementController {
 
-  protected $imageFactory;
-  public function onRespond()
-  {
+    protected $imageFactory;
 
-    \Drupal::service('page_cache_kill_switch')->trigger();
+    public function onRespond()
+    {
 
-    $config = \Drupal::config('pubcid_cookie_management.settings');
+        \Drupal::service('page_cache_kill_switch')->trigger();
 
-    //If the cookie name is empty, then there is nothing to do
-    if (empty($config->get('cookie_name'))) {
-      return $this->imageResponse();
+        $config = \Drupal::config('pubcid_cookie_management.settings');
+
+        //If the cookie name is empty, then there is nothing to do
+        if (empty($config->get('cookie_name'))) {
+            return $this->imageResponse();
+        }
+
+        //Get the setting values
+        $cookie_name = $config->get('cookie_name');
+        $max_age = $config->get('max_age');
+        $cookie_domain = $config->get('cookie_domain');
+        $consent_func = $config->get('consent_function');
+        $gen_func = $config->get('generate_function');
+
+        //Initialize cookie value
+        $value = NULL;
+
+        // See if the cookie exists already
+        if (isset($_COOKIE[$cookie_name])) {
+            $value = $_COOKIE[$cookie_name];
+        }
+
+        // Check consent.  There is consent when
+        // 1. Consent function is empty
+        // 2. Consent function returns true
+        if (!empty($consent_func) && is_callable($consent_func) && !call_user_func($consent_func)) {
+            // Delete old cookie if there is no consent
+            if (isset($value)) {
+                setcookie($cookie_name, '', time() - 3600, '/', $cookie_domain);
+            }
+            return $this->imageResponse();
+        }
+
+        // If the cookie doesn't exist, and there is a generate function,
+        // then call the function to get a value.
+        if (!isset($value)) {
+            if (!empty($gen_func) && is_callable($gen_func)) {
+                $value = call_user_func($gen_func);
+            }
+        }
+
+        // Update the cookie
+        if (isset($value)) {
+            $expires = gmdate('D, d M Y H:i:s T', time() + $max_age * DAYS_IN_SECONDS);
+            header('Set-Cookie: ' . $cookie_name . '=' . $value . '; expires=' . $expires . '; path=/; domain='.$cookie_domain. '; SameSite=Lax' );
+        }
+
+        return $this->imageResponse();
+
     }
 
-    //Get the setting values
-    $cookie_name = $config->get('cookie_name');
-    $max_age = $config->get('max_age');
-    $cookie_path = $config->get('cookie_path');
-    $consent_func = $config->get('consent_function');
-    $gen_func = $config->get('generate_function');
+    /**
+     * Function for the pixel response
+     * @return array|BinaryFileResponse
+     */
+    public function imageResponse() {
 
-    // Obtain site domain if defined
-    if (defined(COOKIE_DOMAIN)) {
-      $cookie_domain = COOKIE_DOMAIN;
-    } else {
-      $cookie_domain = "";
+        $this->imageFactory = \Drupal::service('image.factory');
+
+        $success = file_exists(drupal_get_path('module', 'pubcid_cookie_management').'/image-test.gif');
+        if($success) {
+            $image = $this->imageFactory->get((drupal_get_path('module', 'pubcid_cookie_management').'/image-test.gif'));
+
+            $headers = [
+                'Content-Type' => 'image/gif',
+            ];
+
+            $uri = $image->getSource();
+            if ($uri) {
+                return new BinaryFileResponse($uri, 200, $headers);
+
+            }
+        } else {
+            return array();
+        }
     }
-
-    //Initialize cookie value
-    $value = NULL;
-
-    // See if the cookie exists already
-    if (isset($_COOKIE[$cookie_name])) {
-      $value = $_COOKIE[$cookie_name];
-    }
-
-    // Check consent.  There is consent when
-    // 1. Consent function is empty
-    // 2. Consent function returns true
-    if (!empty($consent_func) && is_callable($consent_func) && !call_user_func($consent_func)) {
-      // Delete old cookie if there is no consent
-      if (isset($value)) {
-        setcookie($cookie_name, '', time() - 3600, $cookie_path, $cookie_domain);
-      }
-      return $this->imageResponse();
-    }
-
-    // If the cookie doesn't exist, and there is a generate function,
-    // then call the function to get a value.
-    if (!isset($value)) {
-      if (!empty($gen_func) && is_callable($gen_func)) {
-        $value = call_user_func($gen_func);
-      }
-    }
-
-    // Update the cookie
-    if (isset($value)) {
-      setcookie($cookie_name, $value, time() + $max_age * 86400, $cookie_path, $cookie_domain);
-    }
-
-    return $this->imageResponse();
-
-  }
-
-  /**
-   * Function for the pixel response
-   * @return array|BinaryFileResponse
-   */
-  public function imageResponse() {
-
-    $this->imageFactory = \Drupal::service('image.factory');
-
-    $success = file_exists(drupal_get_path('module', 'pubcid_cookie_management').'/image-test.gif');
-    if($success) {
-      $image = $this->imageFactory->get((drupal_get_path('module', 'pubcid_cookie_management').'/image-test.gif'));
-
-      $headers = [
-        'Content-Type' => 'image/gif',
-      ];
-
-      $uri = $image->getSource();
-      if ($uri) {
-        return new BinaryFileResponse($uri, 200, $headers);
-
-      }
-    } else {
-      return array();
-    }
-  }
 
 }
 
@@ -102,6 +104,6 @@ class PubcidCookieManagementController {
  * @return uuid
  */
 function pubcid_value_generation() {
-  $uuid_service = \Drupal::service('uuid');
-  return $uuid_service->generate();
+    $uuid_service = \Drupal::service('uuid');
+    return $uuid_service->generate();
 }
--- src/Form/PubcidCookieManagementConfigForm.php
+++ src/Form/PubcidCookieManagementConfigForm.php
@@ -18,133 +18,133 @@ class PubcidCookieManagementConfigForm extends ConfigFormBase
 {
 
 
-  /**
-   * Gets the configuration names that will be editable.
-   *
-   * @return array
-   *   An array of configuration object names that are editable if called in
-   *   conjunction with the trait's config() method.
-   */
-  protected function getEditableConfigNames()
-  {
-    return ['pubcid_cookie_management.settings'];
-  }
-
-
-  /**
-   * Returns a unique string identifying the form.
-   *
-   * The returned ID should be a unique string that can be a valid PHP function
-   * name, since it's used in hook implementation names such as
-   * hook_form_FORM_ID_alter().
-   *
-   * @return string
-   *   The unique string identifying the form.
-   */
-  public function getFormId()
-  {
-    return 'pubcid_cookie_management_settings';
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->config('pubcid_cookie_management.settings');
-
-    //Pubcid Cookie Extender Form
-    $form['pubcid_cookie_extender'] = [
-      '#type' => 'details',
-      '#title' => t('Pubcid Cookie Extender Configuration'),
-      '#open' => TRUE,
-    ];
-
-    //Cookie Name
-    if ($config->get('cookie_name') == '') {
-      $cookie_name_default = 'example: _pubcid';
-    } else {
-      $cookie_name_default = $config->get('cookie_name');
+    /**
+     * Gets the configuration names that will be editable.
+     *
+     * @return array
+     *   An array of configuration object names that are editable if called in
+     *   conjunction with the trait's config() method.
+     */
+    protected function getEditableConfigNames()
+    {
+        return ['pubcid_cookie_management.settings'];
     }
-    $form['pubcid_cookie_extender']['cookie_name'] = [
 
-      '#type' => 'textfield',
-      '#title' => t('Cookie Name'),
-      '#default_value' => $cookie_name_default,
-      '#attributes' => array(
-        'onblur' => "if (this.value == '') {this.value = 'example: _pubcid'}",
-        'onfocus' => "if (this.value == 'example: _pubcid') {this.value = ''}"
-      , ),
-    ];
 
-    //Maximum Age
-    $options = [7, 30, 90, 365];
-    $form['pubcid_cookie_extender']['max_age'] = [
-      '#type' => 'select',
-      '#title' => t('Max Age'),
-      '#default_value' => $config->get('max_age'),
-      '#options' => array_combine($options, $options),
-      '#description' => t('Max Age of the cookie in days'),
-    ];
-
-    //Cookie Path
-    $form['pubcid_cookie_extender']['cookie_path'] = [
-      '#type' => 'textfield',
-      '#title' => t('Cookie Path'),
-      '#default_value' => $config->get('cookie_path'),
-      '#description' => t('Path of the cookie'),
-    ];
-
-    //The Cookie Consent Function
-    if ($config->get('consent_function') == '') {
-      $consent_function_default = 'example: cn_cookie_accept';
-    } else {
-      $consent_function_default = $config->get('consent_function');
+    /**
+     * Returns a unique string identifying the form.
+     *
+     * The returned ID should be a unique string that can be a valid PHP function
+     * name, since it's used in hook implementation names such as
+     * hook_form_FORM_ID_alter().
+     *
+     * @return string
+     *   The unique string identifying the form.
+     */
+    public function getFormId()
+    {
+        return 'pubcid_cookie_management_settings';
     }
-    $form['pubcid_cookie_extender']['consent_function'] = [
-      '#type' => 'textfield',
-      '#title' => t('Consent Function'),
-      '#default_value' => $consent_function_default,
-      '#attributes' => array(
-        'onblur' => "if (this.value == '') {this.value = 'example: cn_cookie_accept'}",
-        'onfocus' => "if (this.value == 'example: cn_cookie_accept') {this.value = ''}"
-      , ),
-      '#description' => t('If specified, then cookie is not updated unless the function returns true.'),
-    ];
 
-    //Cookie Value Generation Function
-    if ($config->get('generate_function') == '') {
-      $generate_function_default = 'example: dp_generate_uuid';
-    } else {
-      $generate_function_default = $config->get('generate_function');
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(array $form, FormStateInterface $form_state) {
+        $config = $this->config('pubcid_cookie_management.settings');
+
+        //Pubcid Cookie Extender Form
+        $form['pubcid_cookie_extender'] = [
+            '#type' => 'details',
+            '#title' => t('Pubcid Cookie Extender Configuration'),
+            '#open' => TRUE,
+        ];
+
+        //Cookie Name
+        if ($config->get('cookie_name') == '') {
+            $cookie_name_default = 'example: _pubcid';
+        } else {
+            $cookie_name_default = $config->get('cookie_name');
+        }
+        $form['pubcid_cookie_extender']['cookie_name'] = [
+
+            '#type' => 'textfield',
+            '#title' => t('Cookie Name'),
+            '#default_value' => $cookie_name_default,
+            '#attributes' => array(
+                'onblur' => "if (this.value == '') {this.value = 'example: _pubcid'}",
+                'onfocus' => "if (this.value == 'example: _pubcid') {this.value = ''}"
+            , ),
+        ];
+
+        //Maximum Age
+        $options = [7, 30, 90, 365];
+        $form['pubcid_cookie_extender']['max_age'] = [
+            '#type' => 'select',
+            '#title' => t('Max Age'),
+            '#default_value' => $config->get('max_age'),
+            '#options' => array_combine($options, $options),
+            '#description' => t('Max Age of the cookie in days'),
+        ];
+
+        //Cookie Path
+        $form['pubcid_cookie_extender']['cookie_domain'] = [
+            '#type' => 'textfield',
+            '#title' => t('Cookie Domain'),
+            '#default_value' => $config->get('cookie_domain'),
+            '#description' => t('Cookie Domain'),
+        ];
+
+        //The Cookie Consent Function
+        if ($config->get('consent_function') == '') {
+            $consent_function_default = 'example: cn_cookie_accept';
+        } else {
+            $consent_function_default = $config->get('consent_function');
+        }
+        $form['pubcid_cookie_extender']['consent_function'] = [
+            '#type' => 'textfield',
+            '#title' => t('Consent Function'),
+            '#default_value' => $consent_function_default,
+            '#attributes' => array(
+                'onblur' => "if (this.value == '') {this.value = 'example: cn_cookie_accept'}",
+                'onfocus' => "if (this.value == 'example: cn_cookie_accept') {this.value = ''}"
+            , ),
+            '#description' => t('If specified, then cookie is not updated unless the function returns true.'),
+        ];
+
+        //Cookie Value Generation Function
+        if ($config->get('generate_function') == '') {
+            $generate_function_default = 'example: dp_generate_uuid';
+        } else {
+            $generate_function_default = $config->get('generate_function');
+        }
+        $form['pubcid_cookie_extender']['generate_function'] = [
+            '#type' => 'textfield',
+            '#title' => t('Generate Function'),
+            '#default_value' => $generate_function_default,
+            '#attributes' => array(
+                'onblur' => "if (this.value == '') {this.value = 'example: dp_generate_uuid'}",
+                'onfocus' => "if (this.value == 'example: dp_generate_uuid') {this.value = ''}"
+            , ),
+            '#description' => t('If specified, then cookie is automatically generated using the value returned by the function.
+                           Enter Drupal\pubcid_cookie_management\Controller\pubcid_value_generation if you want to use the default one'),
+        ];
+
+        return parent::buildForm($form, $form_state);
     }
-    $form['pubcid_cookie_extender']['generate_function'] = [
-      '#type' => 'textfield',
-      '#title' => t('Generate Function'),
-      '#default_value' => $generate_function_default,
-      '#attributes' => array(
-        'onblur' => "if (this.value == '') {this.value = 'example: Drupal\pubcid_cookie_management\EventSubscriber\pubcid_value_generation'}",
-        'onfocus' => "if (this.value == 'example: Drupal\pubcid_cookie_management\EventSubscriber\pubcid_value_generation') {this.value = ''}"
-      , ),
-      '#description' => t('If specified, then cookie is automatically generated using the value returned by the function.
-                           Enter Drupal\pubcid_cookie_management\EventSubscriber\pubcid_value_generation if you want to use the default one'),
-    ];
 
-    return parent::buildForm($form, $form_state);
-  }
+    /**
+     * {@inheritdoc}
+     */
+    public function submitForm(array &$form, FormStateInterface $form_state)
+    {
+        $this->config('pubcid_cookie_management.settings')
+            ->set('cookie_name', $form_state->getValue('cookie_name'))
+            ->set('max_age', $form_state->getValue('max_age'))
+            ->set('cookie_domain', $form_state->getValue('cookie_domain'))
+            ->set('consent_function', $form_state->getValue('consent_function'))
+            ->set('generate_function', $form_state->getValue('generate_function'))
+            ->save();
 
-  /**
-   * {@inheritdoc}
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state)
-  {
-    $this->config('pubcid_cookie_management.settings')
-      ->set('cookie_name', $form_state->getValue('cookie_name'))
-      ->set('max_age', $form_state->getValue('max_age'))
-      ->set('cookie_path', $form_state->getValue('cookie_path'))
-      ->set('consent_function', $form_state->getValue('consent_function'))
-      ->set('generate_function', $form_state->getValue('generate_function'))
-      ->save();
-
-    parent::submitForm($form, $form_state);
-  }
+        parent::submitForm($form, $form_state);
+    }
 }
